cd83d6fb7cacc48eeb30a3c1e4e216412c62f639
Приходько Владислав <3885vlad@gmail.com>
2024-02-17 12:05:41 +0200

    Created base structure

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.lab2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab2.ui.theme.Lab2Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Body()
                 }
             }
         }
@@ -30,17 +36,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Body() {
+//    val image1 = painterResource(R.drawable.ellipse_54);
+
+    Column(
+        modifier = Modifier
+            .padding(4.dp, 0.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(10F)
+        ) {
+            Box(
+                modifier = Modifier
+            ) {
+
+            }
+
+            Column(
+
+            ) {
+
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BodyPreview() {
     Lab2Theme {
-        Greeting("Android")
+        Body()
     }
 }
