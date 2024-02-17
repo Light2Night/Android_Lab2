ce2c42f42bce872b8eaacd87384059068998cf73
Приходько Владислав <3885vlad@gmail.com>
2024-02-17 12:18:39 +0200

    Created buttons

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -3,17 +3,25 @@ package com.example.lab2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab2.ui.theme.Lab2Theme
@@ -41,7 +49,7 @@ fun Body() {
 
     Column(
         modifier = Modifier
-            .padding(4.dp, 0.dp)
+            .padding(10.dp, 0.dp)
     ) {
         Box(
             modifier = Modifier
@@ -49,14 +57,51 @@ fun Body() {
         ) {
             Box(
                 modifier = Modifier
+                    .padding(10.dp)
             ) {
-
+//                Image(painter = , contentDescription = )
             }
 
             Column(
 
             ) {
+                Text(text = "topic")
+                Row {
+                    Text(text = "author")
+                    Text(text = " ")
+                    Text(text = "year")
+                }
+            }
+        }
 
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = { }
+            ) {
+                Text(
+                    modifier = Modifier
+                        .width(80.dp)
+                        .wrapContentSize(Alignment.Center),
+                    text = "Previous",
+                    textAlign = TextAlign.Center
+                )
+            }
+
+            Button(
+                onClick = { }
+            ) {
+                Text(
+                    modifier = Modifier
+                        .width(80.dp)
+                        .wrapContentSize(Alignment.Center),
+                    text = "Next",
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
